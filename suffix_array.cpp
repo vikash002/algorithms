082a65b34880dbc075f3fd9c43240d0b4f5a8d5c
Vikash Kumar <vikash.kr@VS-C02PW7ZJG8WN.local>
2016-12-05 01:00:54 +0530

    suffix array added brutefore

--- suffix_array.cpp
+++ suffix_array.cpp
@@ -4,22 +4,34 @@
 #include <vector>
 #include <utility>
 
-using std::cin;
-using std::cout;
-using std::endl;
-using std::make_pair;
-using std::pair;
-using std::string;
-using std::vector;
+using namespace std;
+#define psi pair<string,int>
+#define f first
+#define s second
+#define mp make_pair
+#define ffor(i,n) for(i = 0; i < n; i++)
+#define rfor(i,n) for(i = n; i >= 0; i--)
 
 // Build suffix array of the string text and
 // return a vector result of the same length as the text
 // such that the value result[i] is the index (0-based)
 // in text where the i-th lexicographically smallest
 // suffix of text starts.
+
+bool comp(const psi& l, const psi& r) {
+	return l.f < r.f;
+}
 vector<int> BuildSuffixArray(const string& text) {
   vector<int> result;
   // Implement this function yourself
+  vector<psi> suffixes;
+  for(int i = 0; i < text.size(); i++) {
+		suffixes.push_back(mp(text.substr(i),i));
+	}
+	sort(suffixes.begin(), suffixes.end(), comp);
+	for(psi tt : suffixes) {
+		result.push_back(tt.s);
+	}
   return result;
 }
 
