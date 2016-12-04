ad25faaac589983b1b4699fda31d48ea92505244
Vikash Kumar <vikash.kr@VS-C02PW7ZJG8WN.local>
2016-12-04 19:43:28 +0530

    Added bruwani-wheels

---
+++ bwt.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string BWT(const string& text) {
+  string result = "";
+  vector<string> cyclicText;
+  // write your code here
+  for(int i = 0; i < text.length(); i++) {
+	  string temp = text.substr(i) + text.substr(0, i);
+	  cyclicText.push_back(temp);
+  }
+  sort(cyclicText.begin(), cyclicText.end());
+  for(int i = 0; i < cyclicText.size(); i++) {
+	  result += cyclicText[i][cyclicText[i].length() -1];
+  }
+
+  return result;
+}
+
+int main() {
+  string text;
+  cin >> text;
+  cout << BWT(text) << endl;
+  return 0;
+}
